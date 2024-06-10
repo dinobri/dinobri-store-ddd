310359a82181167932a16223c0d4e1b6ba4a42f0
Diego Noura de Brito <diego.nobri@gmail.com>
2024-06-10 14:52:31 -0300

    Configuração do projeto e primeiras entdiades

---
+++ src/DinobriStore.Core/DomainObjetcs/IAggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinobriStore.Core.DomainObjetcs
+{
+    public interface IAggregateRoot
+    {
+    }
+}
